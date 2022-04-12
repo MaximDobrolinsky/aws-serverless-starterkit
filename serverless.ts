dbaa80fe8eeebd7a32ef40e21764c346a7abe4c9
Maxim Dobrolinsky <maxim.dobrolinsky@externals.hilti.com>
2022-04-12 16:54:28 +0300

    fix: controller name

--- serverless.ts
+++ serverless.ts
@@ -1,38 +1,38 @@
-import type { AWS } from "@serverless/typescript";
+import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
-  service: "service-composite-thingpayload-v2-lambda",
-  frameworkVersion: "3",
+  service: 'service-composite-thingpayload-v2-lambda',
+  frameworkVersion: '3',
   package: {
     individually: true,
   },
-  variablesResolutionMode: "20210326",
+  variablesResolutionMode: '20210326',
   custom: {
     webpack: {
-      webpackConfig: "./webpack.config.js",
+      webpackConfig: './webpack.config.js',
       includeModules: true,
     },
   },
-  plugins: ["serverless-webpack", "serverless-offline"],
+  plugins: ['serverless-webpack', 'serverless-offline'],
   provider: {
-    name: "aws",
-    runtime: "nodejs14.x",
-    stage: "development",
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    stage: 'development',
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {},
     timeout: 15,
-    region: "us-east-1",
+    region: 'us-east-1',
   },
   functions: {
     application: {
-      handler: "src/controllers/ApplicationController.hello",
+      handler: 'src/controllers/application.hello',
       events: [
         {
           http: {
-            method: "GET",
-            path: "hello",
+            method: 'GET',
+            path: 'hello',
             cors: true,
           },
         },
